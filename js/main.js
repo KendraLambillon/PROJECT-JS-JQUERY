82d1f1b6f26234313a01d78414edbae4e92eb5b6
KendraLa <kendralambillon@gmail.com>
2023-07-09 20:48:53 +0200

    Accordion

--- js/main.js
+++ js/main.js
@@ -1,51 +1,60 @@
 $(document).ready(function(){
 
     //Slider
-    $('.bxslider').bxSlider({
-        mode: 'fade',
-        captions: true,
-        slideWidth: 1250
-    });
+    if(window.location.href.indexOf('index') > -1){
+        $('.bxslider').bxSlider({
+            mode: 'fade',
+            captions: true,
+            slideWidth: 1250,
+            responsive: true,
+            pager: true
+        });
+    }
+
 
     //Posts
-    var posts = [
-        {
-            title: 'Test of title 1',
-            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-            content: 'Lorem ipsum dolor sit amet. Hic corrupti eius aut odit porro ut dignissimos optio cum nihil dolorum non vitae alias.'
-        },
-        {
-            title: 'Test of title 2',
-            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-            content: 'Lorem ipsum dolor sit amet. Hic corrupti eius aut odit porro ut dignissimos optio cum nihil dolorum non vitae alias.'
-        },
-        {
-            title: 'Test of title 3',
-            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-            content: 'Lorem ipsum dolor sit amet. Hic corrupti eius aut odit porro ut dignissimos optio cum nihil dolorum non vitae alias.'
-        },
-        {
-            title: 'Test of title 4',
-            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-            content: 'Lorem ipsum dolor sit amet. Hic corrupti eius aut odit porro ut dignissimos optio cum nihil dolorum non vitae alias.'
-        }
-    ];
+    if(window.location.href.indexOf('index') > -1){
+        var posts = [
+            {
+                title: 'Test of title 1',
+                date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+                content: 'Lorem ipsum dolor sit amet. Hic corrupti eius aut odit porro ut dignissimos optio cum nihil dolorum non vitae alias.'
+            },
+            {
+                title: 'Test of title 2',
+                date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+                content: 'Lorem ipsum dolor sit amet. Hic corrupti eius aut odit porro ut dignissimos optio cum nihil dolorum non vitae alias.'
+            },
+            {
+                title: 'Test of title 3',
+                date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+                content: 'Lorem ipsum dolor sit amet. Hic corrupti eius aut odit porro ut dignissimos optio cum nihil dolorum non vitae alias.'
+            },
+            {
+                title: 'Test of title 4',
+                date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+                content: 'Lorem ipsum dolor sit amet. Hic corrupti eius aut odit porro ut dignissimos optio cum nihil dolorum non vitae alias.'
+            }
+        ];
 
-    //Plantilla de Post
-    posts.forEach((item, index) => {
-        var post = `
-            <article class="post">
-                <h2>${item.title}</h2>
-                <span class="date">${item.date}</span>
-                <p>
-                    ${item.content}
-                </p>
-                <a href="#" class="button-more">Read more</a>
-            </article>
-        `;
+        //Plantilla de Post
+        posts.forEach((item, index) => {
+            var post = `
+                <article class="post">
+                    <h2>${item.title}</h2>
+                    <span class="date">${item.date}</span>
+                    <p>
+                        ${item.content}
+                    </p>
+                    <a href="#" class="button-more">Read more</a>
+                </article>
+            `;
 
-        $("#posts").append(post);
-    });
+            $("#posts").append(post);
+        });
+
+    }
+
 
     //Selector theme
     var theme = $("#theme");
@@ -96,6 +105,11 @@ $(document).ready(function(){
             location.reload(); //Se me actualiza la pagina. location --> me redirige al form de login
         });
     }
+
+    //Acordeon
+    if(window.location.href.indexOf('about') > -1){
+		$("#acordeon").accordion();
+	}
 
 
 });
