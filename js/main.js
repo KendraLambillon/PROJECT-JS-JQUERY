bb8ce4290d1ae294f89e92763d26f2b69f74b8d2
KendraLa <kendralambillon@gmail.com>
2023-07-12 22:50:13 +0200

    Validation contact form

--- js/main.js
+++ js/main.js
@@ -121,6 +121,18 @@ $(document).ready(function(){
         }, 1000);
 	}
 
+    //Validation
+    if(window.location.href.indexOf('contact') > -1){
+        $("form input[name='date']").datepicker({
+			dateFormat: 'dd-mm-yy'
+		});
+
+		$.validate({
+		    lang: 'es',
+		    errorMessagePosition: 'top',
+		    scrollToTopOnError: true
+		});
+    }
 
 });
 
